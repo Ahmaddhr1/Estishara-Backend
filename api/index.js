f161fdfb8d5a3b5bc9cb9c280a96628e43e88841
Ahmaddhr1 <ahmaddaherr004@gmail.com>
2025-02-14 21:27:48 +0200

    ftg

--- api/index.js
+++ api/index.js
@@ -1,4 +1,3 @@
-
 const express = require("express");
 const app = express();
 
@@ -22,8 +21,6 @@ app.get("/users", async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
+
 
 module.exports = app
