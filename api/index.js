96c5e0698f6cea58859fb31985968a7bd4cdec99
Ahmaddhr1 <ahmaddaherr004@gmail.com>
2025-02-14 20:31:18 +0200

    some testing

--- api/index.js
+++ api/index.js
@@ -8,6 +8,15 @@ app.get('/', (req, res) => {
   res.send('Hello, world!');
 });
 
+app.get('/users', async (req, res) => {
+    try {
+      const users = await User.findAll();
+      res.json(users);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  });
+
 
 // Your routes and middleware
 app.listen(3000, () => {
