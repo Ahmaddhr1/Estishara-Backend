8311b4397f7e4120954d0e79c3ce0d8bdf88da3d
Ahmaddhr1 <ahmaddaherr004@gmail.com>
2025-02-15 20:51:01 +0200

    testing deploy

--- api/index.js
+++ api/index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("Hello, estisharaa !");
+  res.send(<h1>Helllooo estisharaaaaaaaaaaaa</h1>);
 });
 
 app.get("/users", async (req, res) => {
