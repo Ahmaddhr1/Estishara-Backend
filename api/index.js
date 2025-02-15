478183c05e48a758ca97711f68e7771a056979b3
Ahmaddhr1 <ahmaddaherr004@gmail.com>
2025-02-15 20:48:56 +0200

    sss

--- api/index.js
+++ api/index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("Hello, world!");
+  res.send("Hello, estisharaa !");
 });
 
 app.get("/users", async (req, res) => {
