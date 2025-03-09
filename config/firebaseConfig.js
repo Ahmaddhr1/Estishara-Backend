e35c0d98e29affbca2be59fb1ba930cc7e1e0310
Ahmaddhr1 <ahmaddaherr004@gmail.com>
2025-03-09 20:58:39 +0200

    dd

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -1,5 +1,5 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./estishara-6d1e0-firebase-adminsdk-fbsvc-4eafc9b4b9.json"); // Path to your JSON file
+const serviceAccount = require("./estishara-6d1e0-firebase-adminsdk-fbsvc-4eafc9b4b9.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
