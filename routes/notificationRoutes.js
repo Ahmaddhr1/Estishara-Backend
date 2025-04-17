b88e440dda286e51543527474ad172d9953d2773
Ahmaddhr1 <ahmaddaherr004@gmail.com>
2025-04-17 16:12:31 +0300

    get not

--- routes/notificationRoutes.js
+++ routes/notificationRoutes.js
@@ -1,11 +1,27 @@
 const express = require("express");
 const router = express.Router();
-const Notification = require("../models/Notification"); 
+const Notification = require("../models/Notification");
 
+router.get("/:id", async (req, res) => {
+  const receiverId = req.params.id;
 
-router.get("/:id", async(req,res)=> {
-    
-})
+  try {
+    const notifications = await Notification.find({
+      receiver: receiverId,
+    }).populate("receiver"); // auto uses receiverModel
+
+    if (!notifications.length) {
+      return res
+        .status(404)
+        .json({ message: "No notifications found for this user" });
+    }
+
+    res.status(200).json(notifications);
+  } catch (error) {
+    console.error("Error fetching notifications:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
 router.post("/", async (req, res) => {
   try {
     const { title, content, receiver, receiverModel } = req.body;
@@ -23,11 +39,13 @@ router.post("/", async (req, res) => {
 
     await newNotification.save();
 
-    res.status(201).json({ message: "Notification sent", notification: newNotification });
+    res
+      .status(201)
+      .json({ message: "Notification sent", notification: newNotification });
   } catch (error) {
     console.error("Notification error:", error);
     res.status(500).json({ message: "Internal server error" });
   }
 });
 
-module.exports =router
+module.exports = router;
