ca2ef7f281f756074e5538bfef0cb63106fbfd6c
Ahmaddhr1 <ahmaddaherr004@gmail.com>
2025-05-01 22:55:22 +0300

    hhh

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -1,11 +1,11 @@
 const admin = require("firebase-admin");
-require("dotenv").config(); // Load environment variables from .env file
+require("dotenv").config(); 
 
 const serviceAccount = {
   type: process.env.FIREBASE_TYPE,
   project_id: process.env.FIREBASE_PROJECT_ID,
   private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'), // Fix new line characters
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
   client_email: process.env.FIREBASE_CLIENT_EMAIL,
   client_id: process.env.FIREBASE_CLIENT_ID,
   auth_uri: process.env.FIREBASE_AUTH_URI,
